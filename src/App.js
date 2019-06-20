55cf983fb6b1d4dc53aa6fff66e9a16ae8adf0e9
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-06-20 00:38:14 -0700

    Creates layout components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
-import {BrowserRouter as Router} from "react-router-dom ";
+import {BrowserRouter as Router} from "react-router-dom";
+
+import Navbar from "./components/layout/Navbar";
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <Navbar />
         <h1>Marioplan</h1>
       </div>
     </Router>
